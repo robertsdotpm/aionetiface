4f45db2f93014e8bad89da8ade26282ae01c6c17
Matthew Roberts <matthew@roberts.pm>
2025-12-25 13:08:58 -0500

    Test remove %scope id and rely on scope field instead.

--- src/aionetiface/net/bind/bind_rules.py
+++ src/aionetiface/net/bind/bind_rules.py
@@ -22,7 +22,7 @@ def get_bind_magic_table(af):
         ["*", IP4, IP_APPEND, V4_VALID_ANY, "0.0.0.0", ""],
 
         # Windows needs the nic no added to v6 private IPs.
-        ["Windows", IP6, IP_APPEND, IP_PRIVATE, "", "nic_id"],
+        #["Windows", IP6, IP_APPEND, IP_PRIVATE, "", "nic_id"],
 
         # ... whereas other operating systems use the interface name.
         ["*", IP6, IP_APPEND, IP_PRIVATE, "", "nic_id"],
